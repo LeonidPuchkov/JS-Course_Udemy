763f71c44fc6f497fc125719434cfc7367e29449
Leonid <leonid.puchkov@gmail.com>
2020-05-12 13:49:13 +0300

    third task

--- js/script.js
+++ js/script.js
@@ -1,29 +1,16 @@
-/* let money = prompt("Ваш бюджет на месяц ?", "Введите сумму");
-let time = prompt("Введите дату в формате YYYY - MM - DD");
 
-let nameExpenses = prompt("Введите обязательную статью расходов в этом месяце");
-let amountExpenses = prompt("Во сколько обойдется?");
+let money, time;
 
-let expenses = {};
-expenses[nameExpenses] = amountExpenses;
-
-let appData = {
-  moneyData: money,
-  timeData: time,
-  expenses: expenses,
-  optionalExpenses: {},
-  income: [],
-  savings: false
-};
-
-var budgetPerDay =
-  (Number(appData.moneyData) - Number(appData.expenses[nameExpenses])) / 30;
-
-alert(budgetPerDay);
- */
-
-let money = prompt("Ваш бюджет на месяц?", ""),
+function start() {
+  money = +prompt("Ваш бюджет на месяц?", "");
   time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+  while (isNaN(money) || money == "" || money == null) {
+    money = +prompt("Ваш бюджет на месяц?", "");
+  }
+}
+
+start();
 
 let appData = {
   budget: money,
@@ -34,19 +21,27 @@ let appData = {
   savings: false
 };
 
-/* for (let i = 0; i < 2; i++) {
-  let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    b = +prompt("Во сколько обойдется?", "");
-  if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null && a != '' && b != '' && a.length < 50) {
-    console.log("done");
-    appData.expenses[a] = b;
-  } else {
-    console.log("Error");
-    i--;
-  }
-} */
 
-let i = 0;
+function chooseExpenses() {
+  for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+      b = +prompt("Во сколько обойдется?", "");
+    if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null && a != '' && b != '' && a.length < 50) {
+      console.log("done");
+      appData.expenses[a] = b;
+    } else {
+      console.log("Error");
+      i--;
+    }
+  }
+}
+
+chooseExpenses();
+
+
+
+
+/* let i = 0;
 do {
   let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
     b = +prompt("Во сколько обойдется?", "");
@@ -59,7 +54,7 @@ do {
   }
   i++;
 } while (i < 2);
-
+ */
 
 /* let i = 0;
 
@@ -68,23 +63,51 @@ while (i < 2) {
     b = +prompt("Во сколько обойдется?", "");
   if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
     console.log("done");
-    i++;
     appData.expenses[a] = b;
   } else {
     console.log("Error");
+    i--;
   }
+  i++;
 } */
 
-appData.moneyPerDay = appData.budget / 30;
-
-alert("Ежедневный бюджет: " + appData.moneyPerDay);
-
-if (appData.moneyPerDay < 100) {
-  console.log("Минимальный уровень достатка");
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-  console.log("Средний уровень достатка");
-} else if (appData.moneyPerDay > 2000) {
-  console.log("Высокий уровень достатка");
-} else {
-  console.log("Произошла ошибка");
+function detectDayBudget() {
+  appData.moneyPerDay = (appData.budget / 30).toFixed();
+  alert("Ежедневный бюджет: " + appData.moneyPerDay);
 }
+
+detectDayBudget();
+
+function detectLevel() {
+  if (appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+  } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+  } else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+  } else {
+    console.log("Произошла ошибка");
+  }
+}
+
+detectLevel();
+
+function checkSavings() {
+  if (appData.savings == true) {
+    let save = +prompt("Какова сумма накоплений?"),
+      percent = +prompt("Под какой процент?");
+
+    appData.monthIncome = save / 100 / 12 * percent;
+    alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
+  }
+}
+
+checkSavings();
+
+function chooseOptExpenses() {
+  for (let i = 1; i < 3; i++) {
+    let a = +prompt("Статья необязательных расходов?", "")
+    appData.optionalExpenses[i] = a;
+  }
+}
+/* chooseOptExpenses(); */
